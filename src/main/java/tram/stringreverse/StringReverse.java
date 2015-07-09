c7f34eb727df76adbaed541ed33b63e92e59abc8
Tram <tramphan415@gmail.com>
2015-07-08 18:54:52 -0700

    create new class

--- src/main/java/tram/stringreverse/StringReverse.java
+++ src/main/java/tram/stringreverse/StringReverse.java
@@ -4,4 +4,7 @@ package tram.stringreverse;
  * Created by Tram on 7/8/2015.
  */
 public class StringReverse {
+    public String reverse(String text) {
+        return new StringBuilder(text).reverse().toString();
+    }
 }
